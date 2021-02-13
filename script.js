e2a72662e814065af4b77686a30aa75d225265d1
laortizt <adri26.ortiz@gmail.com>
2021-02-13 13:08:12 -0500

    ok

--- script.js
+++ script.js
@@ -33,7 +33,33 @@ const getPokemon = async (id) => {
 }
 
 const createPokemonCard = (pokemon) => {
-    
+	//aqui en adelante
+	const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span> </small>
+    </div>
+    `
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    poke_container.appendChild(pokemonEl)
 }
 
 fetchPokemons()
